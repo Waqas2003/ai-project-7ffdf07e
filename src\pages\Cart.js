ec10b81b1e2dea537858a610b45b0f94374864b9
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-04 18:59:20 +0500

    Add src\pages\Cart.js

---
+++ src\pages\Cart.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Container, Grid, Typography, Button } from '@material-ui/core';
+import { useSelector } from 'react-redux';
+import { cartItems } from '../store/cartSlice';
+
+function Cart() {
+  const cart = useSelector(cartItems);
+
+  return (
+    <Container maxWidth="md">
+      <Grid container spacing={2}>
+        {cart.map((item) => (
+          <Grid item key={item.id} xs={12} sm={6}>
+            <Typography variant="body1">{item.name}</Typography>
+            <Typography variant="body1">${item.price}</Typography>
+            <Typography variant="body1">Quantity: {item.quantity}</Typography>
+          </Grid>
+        ))}
+        <Grid item xs={12}>
+          <Typography variant="h5">Total: ${cart.reduce((acc, item) => acc + item.price * item.quantity, 0)}</Typography>
+          <Button variant="contained" color="primary">
+            Checkout
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
+
+export default Cart;
