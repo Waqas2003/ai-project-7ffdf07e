b9b528509884eed01d3446d5501ed5ae7d73d376
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-04 18:59:12 +0500

    Add src\data.js

---
+++ src\data.js
@@ -0,0 +1,25 @@
+const products = [
+  {
+    id: 1,
+    name: 'Product 1',
+        price: 10.99,
+    image: 'https://via.placeholder.com/300',
+  },
+  {
+    id: 2,
+    name: 'Product 2',
+        price: 9.99,
+    image: 'https://via.placeholder.com/300',
+  },
+  {
+    id: 3,
+    name: 'Product 3',
+        price: 12.99,
+    image: 'https://via.placeholder.com/300',
+  },
+];
+
+export { products };
+```
+
+This is a basic e-commerce website using React, Redux, and Material-UI. It has a home page that displays a list of products, a product detail page that shows the product information, a cart page that displays the items in the cart, and a checkout page that shows the order summary. The website uses Redux to manage the state of the cart.
